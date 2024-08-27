a7eec39a4b64c496519617371cfeec747004e330
xeo3221 <95309495+xeo3221@users.noreply.github.com>
2024-08-27 11:43:45 +0200

    Initial commit

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -5,7 +5,7 @@ const ErrorPage = () => {
   return (
     <div className="flex items-center justify-center hc w-full">
       <h4 className=" text-center text-lg font-medium">
-        It seems that you have git a page that does not exist. Please go back to
+        It seems that you have hit a page that does not exist. Please go back to
         the home page.
       </h4>
     </div>
